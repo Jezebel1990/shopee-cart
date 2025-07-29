f88db96dfc49b42c855c41ce02660ab8e2e0d95f
Jezebel1990 <jezebelguedes@gmail.com>
2025-07-29 15:50:49 -0300

    feat: add delete item

--- src/index.js
+++ src/index.js
@@ -12,5 +12,7 @@ const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myWhishList, item2);
 
+await cartService.deleteItem(myCart, item1.name);
+
 console.log("Shopee Cart TOTAL IS:");
 await cartService.calculateTotal(myCart);
